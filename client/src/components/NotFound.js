87ff3c2670ab25613d7b19d2a6d8578207d26fa3
kaoutar1987 <kaoutarhabach@gmail.com>
2021-03-30 23:36:30 +0100

    create coponents

---
+++ client/src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () =>{
+
+    return <p>Inside NotFound component</p>   
+            
+}
+
+export default NotFound;
---
+++ client/src/components/Signin.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Signin = () =>{
+
+    return <p>Inside Signin component</p>   
+            
+}
+
+export default Signin;
